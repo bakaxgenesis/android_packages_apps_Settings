09c8079b607972e015f3b9974fa3bc1daa5753ee
baka-hokage <ayushbehera007@gmail.com>
2025-01-27 18:09:17 +0000

    update

--- src/com/android/settings/security/HideDeveloperStatusPreferenceController.java
+++ src/com/android/settings/security/HideDeveloperStatusPreferenceController.java
@@ -18,19 +18,18 @@ package com.android.settings.security;
 
 import android.content.Context;
 import android.content.pm.UserInfo;
-import android.provider.Settings;
 import android.os.UserManager;
 
-import com.android.settings.core.BasePreferenceController;
-
 import com.android.internal.util.genesis.HideDeveloperStatusUtils;
+import com.android.settings.core.BasePreferenceController;
 
 import java.util.List;
 
 public class HideDeveloperStatusPreferenceController extends BasePreferenceController {
 
     private static final String PREF_KEY = "hide_developer_status_settings";
-    private static HideDeveloperStatusUtils hideDeveloperStatusUtils = new HideDeveloperStatusUtils();
+    private static HideDeveloperStatusUtils hideDeveloperStatusUtils =
+            new HideDeveloperStatusUtils();
 
     private UserManager userManager;
     private List<UserInfo> userInfos;
@@ -39,7 +38,7 @@ public class HideDeveloperStatusPreferenceController extends BasePreferenceContr
         super(context, PREF_KEY);
         userManager = UserManager.get(context);
         userInfos = userManager.getUsers();
-        for (UserInfo info: userInfos) {
+        for (UserInfo info : userInfos) {
             hideDeveloperStatusUtils.setApps(context, info.id);
         }
     }
